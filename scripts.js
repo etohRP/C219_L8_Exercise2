638d6fca2664853edd91b37f3744d0e95fa9fc78
etohRP <22016439@myrp.edu.sg>
2024-01-02 15:27:13 +0800

    first

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#f2e0ff', '#a986c2', '#603c7a'],
+        anchors: ['section1', 'section2', 'section3'],
+        menu: '#menu',
+        navigation: true,
+        navigationPosition: 'right',
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+    });
+});
